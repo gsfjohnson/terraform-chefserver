e31610ea539b0a904383ed56dc58c993c701e126
Glen Johnson <gsfjohnson@gmail.com>
2016-05-23 22:15:07 -0800

    fix install script mistake

--- chef-server-install.sh
+++ chef-server-install.sh
@@ -2,12 +2,11 @@
 
 hostnamectl set-hostname ${fqdn}
 
-yum -y git
+yum -y install git
+yum -y install https://packages.chef.io/stable/el/7/chef-server-core-12.6.0-1.el7.x86_64.rpm >>/root/chef-install.log
 
 #git clone https://github.com/certbot/certbot
 #cd certbot
 #./certbot-auto -n -v >>/root/chef-install.log
-
-yum -y install https://packages.chef.io/stable/el/7/chef-server-core-12.6.0-1.el7.x86_64.rpm >>/root/chef-install.log
 
 chef-server-ctl reconfigure >>/root/chef-install.log
